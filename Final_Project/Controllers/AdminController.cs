4972586b0f479902a144350d73db6771c8aa2302
Mahmoud-Elgammal-129 <MahmoudElgammal129@gmail.com>
2023-07-16 23:25:52 +0300

    Add project files.

---
+++ Final_Project/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Final_Project.Controllers
+{
+    [Authorize(Roles = "Admin")]
+
+    public class AdminController : Controller
+    {
+        
+        public IActionResult Home_Admin()
+        {
+            return View();
+        }
+    }
+}
